c5ebce17b80b8bb84eb7665140bc4d94dd93f9ce
kerryRarson <klarsen@w-link.net>
2014-12-22 17:17:35 -0700

    added GeoCode api controller

---
+++ OPI.HHS.insight/web/OPI.HHS.Insight/Controllers/api/GeoCodeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml;
+using System.Xml.Linq;
+using OPI.HHS.Insight.Models;
+
+namespace OPI.HHS.Insight.Controllers.api
+{
+    public class GeoCodeController : ApiController
+    {
+        [HttpPost]
+        [Route("api/geocode")]
+        public LatLon GeoCode(string line1, string line2, string city, string state)
+        {
+            LatLon rtn = null;
+            var serviceUri = string.Format("http://maps.googleapis.com/maps/api/geocode/xml?address={0}&sensor=false", Uri.EscapeDataString(String.Format("{0}, {1}, {2}, {3}", line1, line2, city, state)));
+            var googleRequest = WebRequest.Create(serviceUri);
+            var response = googleRequest.GetResponse();
+            var xdoc = XDocument.Load(response.GetResponseStream());
+            var result = xdoc.Element("GeocodeResponse").Element("result");
+            if (result != null)
+            {
+                var locationElement = result.Element("geometry").Element("location");
+                rtn = new LatLon {
+                    Lat = locationElement.Element("lat").Value,
+                    Lon = locationElement.Element("lng").Value,
+                    FormattedAddress = result.Element("formatted_address").Value
+                };
+                if (result.Element("partial_match") != null)
+                {
+                    if (result.Element("partial_match").Value == "true") { rtn.PartialMatch = true; }
+                }
+            }
+
+            return rtn;
+        }
+    }
+}
---
+++ OPI.HHS.insight/web/OPI.HHS.Insight/Models/LatLon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPI.HHS.Insight.Models
+{
+    public class LatLon
+    {
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public bool PartialMatch { get; set; }
+        public string FormattedAddress { get; set; }
+    }
+}
